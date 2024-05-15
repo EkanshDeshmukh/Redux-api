534bd0a1ef5fb01de333c82a209c21ebd809265e
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2024-05-15 22:11:55 +0530

    Added userslist

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { userSlice } from './reducers/userReducer'
+import userReducer from './reducers/userReducer'
 
 export const store = configureStore({
     reducer: {
-        userSlice: userSlice
+         userReducer:userReducer
     },
-})
+}) 
